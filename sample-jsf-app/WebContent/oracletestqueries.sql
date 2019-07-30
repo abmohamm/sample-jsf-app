574a2edb0e2e56bf665c625e56697dab8b91960d
abmohamm <mohd.92java@gmail.com>
2019-07-29 17:10:00 -0700

    [US1234]:Adding data load from one table to other queries.

--- sample-jsf-app/WebContent/oracletestqueries.sql
+++ sample-jsf-app/WebContent/oracletestqueries.sql
@@ -21,6 +21,8 @@ select * from employees where hire_date<to_date('24-07-05','dd-mm-yy')
 
 select * from tab
 
+selet SQRT(81) from dual
+
 select * from employees;
 
 select * from employees where first_name like '_%a'
@@ -59,3 +61,23 @@ select round(10.266,2) from dual;
 select trunc(10.266,2) from dual;
 
 select sysdate from dual;
+
+desc user_constraints
+
+select constraint_name,constraint_type from user_constraints where table_name='employees'
+
+desc user_cons_columns 
+
+select * from employees;
+
+select * from NEWTABLE;
+
+merge into NEWTABLE n using EMPLOYEES e on (e.employee_id=n.employee_id)
+when matched then
+update set n.first_name=e.first_name,n.last_name=e.last_name,n.email=e.email,n.phone_number=e.phone_number,n.hire_date=e.hire_date,
+n.job_id=e.job_id,n.salary=e.salary,n.commission_pct=e.commission_pct,n.manager_id=e.manager_id,n.department_id=e.department_id,
+n.creat_upd_date=e.creat_upd_date
+when not matched then
+insert values(e.employee_id,e.first_name,e.last_name,e.email,e.phone_number,e.hire_date,e.job_id,e.salary,e.commission_pct,
+e.manager_id,e.department_id,current_timestamp)
+
